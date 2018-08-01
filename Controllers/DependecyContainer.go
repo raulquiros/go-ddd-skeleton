1b75038e095a4bd65e3bd1cdf799c02d356f002e
Raúl Quiros Mayer <rraulqm92@gmail.com>
2018-08-01 23:27:41 +0200

    DependencyContainer package

--- Controllers/DependecyContainer.go
+++ Controllers/DependecyContainer.go
@@ -2,16 +2,17 @@ package Controllers
 
 import (
 	"github.com/codegangsta/inject"
-}
+	"go-ddd-structure/Infrastructure/Model"
+)
 
 // Dependency Container
 type DependencyContainer struct {
-	Db    *Model.MongoLoggerRepository `inject`
+	Db *Model.MongoLoggerRepository `inject`
 }
 
 func GetInjector() inject.Injector {
 	injector := inject.New()
-	injector.Map(&Model.MongoLoggerRepository{"127.0.0.1","versionControl","logs"})
+	injector.Map(&Model.MongoLoggerRepository{"127.0.0.1", "versionControl", "logs"})
 	return injector
 }
 
@@ -19,4 +20,3 @@ func GetContainer(container interface{}) error {
 	injector := GetInjector()
 	return injector.Apply(container)
 }
-
